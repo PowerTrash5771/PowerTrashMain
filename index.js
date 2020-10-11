40f2875208931d7db86035601652cdc276a51bb2
PowerTrash5771 <72710149+PowerTrash5771@users.noreply.github.com>
2020-10-11 18:00:43 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+/*var subcount = document.querySelector('#yt-subscribe > span > span.yt-subscription-button-subscriber-count-branded-horizontal').innerHTML
+document.querySelectorAll('center')[0].style.display = "none";
+document.querySelectorAll('.channels .subcount')[0].innerHTML = subcount;*/
