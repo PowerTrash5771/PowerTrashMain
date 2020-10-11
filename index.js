9cd00e6bcf4bae8eeceed2193a9e4bf64591bee8
PowerTrash5771 <72710149+PowerTrash5771@users.noreply.github.com>
2020-10-11 18:06:54 -0500

    Delete index.js

--- index.js
+++
@@ -1,3 +0,0 @@
-/*var subcount = document.querySelector('#yt-subscribe > span > span.yt-subscription-button-subscriber-count-branded-horizontal').innerHTML
-document.querySelectorAll('center')[0].style.display = "none";
-document.querySelectorAll('.channels .subcount')[0].innerHTML = subcount;*/
